a2f5f9f97f2ac0273de76a85ba0e39030e859aae
natamate <naciaa.nm@gmail.com>
2017-06-26 09:14:45 +0200

    Update replaceOrRemoveAllAcuresOfChar.c

--- replaceOrRemoveAllAcuresOfChar.c
+++ replaceOrRemoveAllAcuresOfChar.c
@@ -1,3 +1,43 @@
+//ASSembler to function removeAllOcuresOfCharFromStringA
+	.text
+	.type removeAllOcuresOfCharFromStringA, @function
+	.global removeAllOcuresOfCharFromStringA
+removeAllOcuresOfCharFromStringA:	
+        pushq   %rbp
+        movq    %rsp, %rbp
+        movq    %rdi, -24(%rbp)
+        movl    %esi, %eax
+        movb    %al, -28(%rbp)
+        movq    -24(%rbp), %rax
+        movq    %rax, -16(%rbp)
+        movq    -16(%rbp), %rax
+        movq    %rax, -8(%rbp)
+.L3:
+        movq    -8(%rbp), %rax
+        movzbl  (%rax), %edx
+        movq    -16(%rbp), %rax
+        movb    %dl, (%rax)
+        movq    -16(%rbp), %rax
+        movzbl  (%rax), %eax
+        testb   %al, %al
+        setne   %al
+        testb   %al, %al
+        je      .L2
+        movq    -8(%rbp), %rax
+        leaq    1(%rax), %rdx
+        movq    %rdx, -8(%rbp)
+        movzbl  (%rax), %eax
+        cmpb    %al, -28(%rbp)
+        setne   %al
+        movzbl  %al, %eax
+        addq    %rax, -16(%rbp)
+        jmp     .L3
+.L2:
+        movq    -24(%rbp), %rax
+        popq    %rbp
+        ret
+        
+
 #include <stdio.h>
 #include <string.h>
 
