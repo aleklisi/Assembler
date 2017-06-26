0a841f7bd0b59255d086dd55bef6e642946a83d7
natamate <naciaa.nm@gmail.com>
2017-06-26 09:14:00 +0200

    Update replaceOrRemoveAllAcuresOfChar.c

--- replaceOrRemoveAllAcuresOfChar.c
+++ replaceOrRemoveAllAcuresOfChar.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+//To compile&link: gcc -o lab_7b lab_7b.c lab_7b_asm.s
+// To run:          ./lab_7b
+
+char* removeAllOcuresOfCharFromStringA(char* str, char c);
+
 char* removeAllOcuresOfCharFromString(char* str, char c) {
     char* s;
     char* d;
@@ -22,5 +27,7 @@ int main(){
 char test1 [] = "asdfafdsa";
 char test2 [] = "asdfafdsa";
 printf("%s\n",replaceAllOcuresOfCharFromString(test1,'a','z'));
-printf("%s\n",removeAllOcuresOfCharFromString(test2,'a'));    
+printf("%s\n",removeAllOcuresOfCharFromString(test2,'a'));  
+printf("%s\n",removeAllOcuresOfCharFromStringA(test1,'a'));  
+return 0;
 }
